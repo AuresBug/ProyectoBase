521695feceab4d17d211c0adba28ad12158ca78e
Hector Anaya <anayac.hector@gmail.com>
2024-09-26 16:52:17 +0000

    save

--- app/Http/Requests/User/UpdateUserRequest.php
+++ app/Http/Requests/User/UpdateUserRequest.php
@@ -24,12 +24,28 @@ class UpdateUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'     => 'required|string',
-            'email'    => 'required|unique:users,email,' . $this->user->id,
-            'password' => 'nullable|confirmed',
-            'avatar'   => 'sometimes|mimes:png,jpg',
-            'roles.*'  => 'required|exists:roles,id',
-
+            'name' => 'required|string|max:255',                                       // Limita la longitud del nombre
+            'email' => 'required|email|max:255|unique:users,email,' . $this->user->id, // Verifica la unicidad del email, exceptuando el del usuario actual
+            'password' => 'nullable|min:8|confirmed',                                  // La contraseña es opcional, pero debe tener al menos 8 caracteres y confirmarse
+            'avatar' => 'nullable|image|mimes:png,jpg,jpeg|max:2048',                  // Verifica que sea una imagen y limita el tamaño a 2MB
+            'roles.*' => 'required|exists:roles,id',                                   // Asegura que los roles existen en la tabla 'roles'
         ];
     }
+
+/**
+ * Get custom attribute names for validation.
+ *
+ * @return array
+ */
+    public function attributes()
+    {
+        return [
+            'name'     => 'nombre',
+            'email'    => 'correo electrónico',
+            'password' => 'contraseña',
+            'avatar'   => 'avatar',
+            'roles.*'  => 'roles',
+        ];
+    }
+
 }
